cf307091e45a34dd8340efca1d3d00aada8b6b3c
NathanEscandor <nathan.escandor@seequent.com>
2021-01-05 12:42:39 -0700

    Removed codon demo for now

--- src/Content/Projects.js
+++ src/Content/Projects.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Grid, Divider } from '@material-ui/core';
-import CodonDemo from './CodonDemo/CodonDemo';
 
 function Projects() {
     return(
@@ -11,13 +10,11 @@ function Projects() {
                         Central-Dogma is a little package that I wrote to practice my JS skills and learn about npm packages. It's used to convert DNA sequences to RNA and then to Amino Acids.
                     </p>
                     <p>
-                        I'd like to re-write the package to bring it up to date with current ES-262 methods but for now, here's a React demo showing some of its features!
+                        In the future, I'd like to use my recently acquired experience to refactor it to fit modern EcmaScript methods.
                     </p>
                     <p><a href="https://github.com/NathanEscandor/codon">GITHUB REPO</a> | <a href="https://www.npmjs.com/package/central-dogma">NPM PAGE</a></p>
                 <br /><br />
             </Grid>
-            <Divider />
-            <CodonDemo />
         </>
     );
 };
